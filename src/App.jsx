cf725a6edfca939fe719f4051f468760cd30c824
Rao Danish <danishrao299@gmail.com>
2024-12-09 23:27:26 +0500

    some ui interface things updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 import { BrowserRouter, Routes, Route } from "react-router"
 import Home from './components/ui/Home'
+import Register from './auth/register/register'
+
 function App() {
   return (
     <BrowserRouter>
       {/* <Header/> */}
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/register" element={<Register />} />
+         {/* 
+        <Route path="/user" element={<User />} />
         <Route path="/user" element={<User />} />
         <Route path="/teacher" element={<Teacher />} /> */}
       </Routes>
---
+++ src/auth/login/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <View>
+      <Text>login</Text>
+    </View>
+  )
+}
+
+export default Login
