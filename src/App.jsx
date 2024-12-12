603883d02d73a80ccbab5f95939fc54f3f7406e6
Anas raza <154615716+Muhammadanasraza@users.noreply.github.com>
2024-12-13 01:05:26 +0500

    Design Main Page Of Website

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router"
 import Home from './components/Home'
 import Header from './components/Header'
+import Dashboard from './components/Dashboard'
 
 
 function App() {
   return (
     <BrowserRouter>
+    
       <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        
+        <Route path="/dashboard" element={<Dashboard/>} />
         {/* <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/user" element={<User />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,51 +1,124 @@
 
 import React, { useState } from "react";
 import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+import { Link } from "react-router";
+
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const isLogin = true;
 
   return (
-    <header className="text-gray-600 body-font">
-  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-        viewBox="0 0 24 24"
-      >
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-      </svg>
-      <span className="ml-3 text-xl">Tailblocks</span>
-    </a>
-    <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-gray-900">Home</a>
-      <a className="mr-5 hover:text-gray-900">About</a>
-      <a className="mr-5 hover:text-gray-900">Courses</a>
-      <a className="mr-5 hover:text-gray-900">Teachers</a>
-    </nav>
-    <Button className="max-sm:hidden">Login
-      <svg
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        className="w-4 h-4 ml-1"
-        viewBox="0 0 24 24"
-      >
-        <path d="M5 12h14M12 5l7 7-7 7" />
-      </svg>
-    </Button>
-  </div>
-</header>
+    <nav className="bg-gray-100 items-center mx-auto border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl  flex flex-wrap items-center justify-between mx-auto p-4">
+      <a
+                        href="#"
+                        className="flex items-center space-x-3 rtl:space-x-reverse"
+                    >
+                        <img
+                                src="https://img.freepik.com/free-photo/view-3d-book-with-graduation-cap_23-2151103704.jpg?ga=GA1.1.518592586.1717923796&semt=ais_hybrid"
+                                className="h-8 rounded-full"
+                                alt="Flowbite Logo"
+                            />
+                        <span className="self-center font-extrabold text-2xl  whitespace-nowrap dark:text-white">
+                            M-System
+                        </span>
+                    </a>
+                    {
+                      isLogin ? 
+                      <DropdownMenu className="cursor-pointer">
 
-   
+                      <DropdownMenuTrigger asChild>
+                        {/* <Button variant="outline">Open</Button> */}
+                        <Avatar>
+                          <AvatarImage className="cursor-pointer" src="https://img.freepik.com/free-photo/portrait-curly-headed-man-wearing-casual-maroon-t-shirt_176532-8142.jpg?ga=GA1.1.518592586.1717923796&semt=ais_hybrid" alt="@shadcn" />
+                          <AvatarFallback>pic</AvatarFallback>
+                        </Avatar>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent className="w-56">
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuGroup>
+                          <a href="/">
+                            <DropdownMenuItem>
+                              Home
+                              <DropdownMenuShortcut>⇧⌘</DropdownMenuShortcut>
+                            </DropdownMenuItem>
+                          </a>
+                          <a href="/dashboard">
+                            <DropdownMenuItem>
+                              Dachboard
+                              <DropdownMenuShortcut>⌘</DropdownMenuShortcut>
+                            </DropdownMenuItem>
+                          </a>
+                          <a href="/profile">
+                            <DropdownMenuItem>
+                              Profile
+                              <DropdownMenuShortcut>⇧⌘</DropdownMenuShortcut>
+                            </DropdownMenuItem>
+                          </a>
+                         
+            
+                          <a href="/appointment">
+                            <DropdownMenuItem>
+                              My Appointment
+                              <DropdownMenuShortcut>⌘</DropdownMenuShortcut>
+                            </DropdownMenuItem>
+                          </a>
+                        </DropdownMenuGroup>
+                        <DropdownMenuSeparator />
+            
+                        <form
+            
+                        >
+                          <button className="pl-2 hover:bg-slate-100 rounded-sm w-full text-left" type="submit"> Logout</button>
+                        </form>
+            
+            
+            
+                      </DropdownMenuContent>
+            
+                    </DropdownMenu>
+                    : 
+                    <Button className="max-sm:hidden">Login
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      className="w-4 h-4 ml-1"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M5 12h14M12 5l7 7-7 7" />
+                    </svg>
+                  </Button>
+                    }
+       
+       
+      </div>
+    </nav>
+
+
   );
 };
 
