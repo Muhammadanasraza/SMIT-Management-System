aa584094479aa1320b988e312d61928b703c70fc
Rao Danish <danishrao299@gmail.com>
2024-12-15 10:16:55 +0500

    text change

--- src/auth/login/login.jsx
+++ src/auth/login/login.jsx
@@ -104,7 +104,7 @@ const Login = () => {
                           />
                         </svg>
                       </div>
-                      <span className="ml-4">Sign Ip with Google</span>
+                      <span className="ml-4">Sign In with Google</span>
                     </button>
                     {/* <button className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5">
              <div className="bg-white p-1 rounded-full">
