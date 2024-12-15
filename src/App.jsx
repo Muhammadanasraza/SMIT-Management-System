e68e8d4c679fef65294de76803175f7dfab22149
Rao Danish <danishrao299@gmail.com>
2024-12-15 09:25:24 +0500

    auth-ui and login with google has been done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { BrowserRouter, Routes, Route } from "react-router"
 import Home from './components/ui/Home'
+import Login from './auth/login/login'
+import Register from './auth/register/register'
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       {/* <Header/> */}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
          {/* 
         <Route path="/user" element={<User />} />
         <Route path="/user" element={<User />} />
